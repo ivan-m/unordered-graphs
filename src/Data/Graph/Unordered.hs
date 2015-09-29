e870bdf24c5958d5d0ee4f3777cc612b05a3441a
Ivan Lazar Miljenovic <ivan.miljenovic@gmail.com>
2015-09-29 13:10:20 +1000

    Initial library setup

---
+++ src/Data/Graph/Unordered.hs
@@ -0,0 +1,11 @@
+{- |
+   Module      : Data.Graph.Unordered
+   Description : Graphs with Hashable nodes
+   Copyright   : (c) Ivan Lazar Miljenovic
+   License     : MIT
+   Maintainer  : Ivan.Miljenovic@gmail.com
+
+
+
+ -}
+module Data.Graph.Unordered where
