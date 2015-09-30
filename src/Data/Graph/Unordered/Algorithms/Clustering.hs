0bbc8b371332dfc3d424d68dee33608f93a849eb
Ivan Lazar Miljenovic <ivan.miljenovic@gmail.com>
2015-10-01 08:50:38 +1000

    Start work on algorithms

---
+++ src/Data/Graph/Unordered/Algorithms/Clustering.hs
@@ -0,0 +1,11 @@
+{- |
+   Module      : Data.Graph.Unordered.Algorithms.Clustering
+   Description : Graph partitioning
+   Copyright   : (c) Ivan Lazar Miljenovic
+   License     : MIT
+   Maintainer  : Ivan.Miljenovic@gmail.com
+
+
+
+ -}
+module Data.Graph.Unordered.Algorithms.Clustering where
