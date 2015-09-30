d864c66718ab4f1180e341dbd1ffe020b7205ff0
Ivan Lazar Miljenovic <ivan.miljenovic@gmail.com>
2015-09-30 16:22:18 +1000

    Can no add and delete nodes and edges

--- src/Data/Graph/Unordered.hs
+++ src/Data/Graph/Unordered.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ConstraintKinds, FlexibleContexts, FlexibleInstances,
              GeneralizedNewtypeDeriving, MultiParamTypeClasses,
-             StandaloneDeriving, TupleSections, TypeFamilies, ViewPatterns #-}
+             StandaloneDeriving, TupleSections, TypeFamilies #-}
 
 {- |
    Module      : Data.Graph.Unordered
@@ -76,6 +76,9 @@ class (NodeFrom (AdjType et)) => EdgeType et where
 
   toEdge :: n -> AdjType et n -> et n
 
+  -- | Returns a list of length 2.
+  edgeNodes :: et n -> [n]
+
 class NodeFrom at where
   getNode :: at n -> n
 
@@ -103,6 +106,8 @@ instance EdgeType DirEdge where
   toEdge u (ToNode   v) = DE u v
   toEdge v (FromNode u) = DE u v
 
+  edgeNodes (DE u v) = [u,v]
+
 instance EdgeType UndirEdge where
   type AdjType UndirEdge = Identity
 
@@ -111,6 +116,8 @@ instance EdgeType UndirEdge where
   otherN n (UE vs) = Identity $ head (delete n vs)
 
   toEdge u (Identity v) = UE [u,v]
+
+  edgeNodes = ueElem
 
 -- -----------------------------------------------------------------------------
 
@@ -200,21 +207,19 @@ isEmpty = M.null . nodeMap
 
 -- -----------------------------------------------------------------------------
 
-type Matchable et n nl el ctxt = (Hashable n
-                                 ,Eq n
-                                 ,EdgeType et
+type Matchable et n nl el ctxt = (ValidGraph et n nl el
                                  ,FromContext ctxt
                                  ,ValidContext et n nl el ctxt
                                  )
 
-match :: (Matchable et n nl el ctxt) => n -> Graph et n nl el
-         -> Maybe (ctxt, Graph et n nl el)
+match :: (ValidGraph et n nl el) => n -> Graph et n nl el
+         -> Maybe (Context (AdjType et) n nl el, Graph et n nl el)
 match n g = getCtxt <$> M.lookup n nm
   where
     nm = nodeMap g
     em = edgeMap g
 
-    getCtxt (nl,adj) = (fromContext ctxt, g')
+    getCtxt (nl,adj) = (ctxt, g')
       where
         ctxt = Ctxt n nl adjM
         -- TODO: what about loops? will only appear once here...
@@ -233,25 +238,31 @@ match n g = getCtxt <$> M.lookup n nm
                      (M.delete n nm)
                      adjNs
 
-matchAny :: (Matchable et n nl el ctxt) => Graph et n nl el
-            -> Maybe (ctxt, Graph et n nl el)
+matchAs :: (Matchable et n nl el ctxt) => n -> Graph et n nl el
+           -> Maybe (ctxt, Graph et n nl el)
+matchAs n = fmap (first fromContext) . match n
+
+matchAny :: (ValidGraph et n nl el) => Graph et n nl el
+            -> Maybe (Context (AdjType et) n nl el, Graph et n nl el)
 matchAny g
   | isEmpty g = Nothing
   | otherwise = flip match g . head . M.keys $ nodeMap g
 
+matchAnyAs :: (Matchable et n nl el ctxt) => Graph et n nl el
+              -> Maybe (ctxt, Graph et n nl el)
+matchAnyAs = fmap (first fromContext) . matchAny
+
 -- -----------------------------------------------------------------------------
 
-type Mergeable et n nl el ctxt = (Hashable n
-                                 ,Eq n
-                                 ,EdgeType et
+type Mergeable et n nl el ctxt = (ValidGraph et n nl el
                                  ,ToContext ctxt
                                  ,ValidContext et n nl el ctxt
                                  )
 
 -- Assumes edge identifiers are valid
-merge :: (Mergeable et n nl el ctxt) => ctxt -> Graph et n nl el
-         -> Graph et n nl el
-merge (toContext -> ctxt) g = Gr nm' em' nextE'
+merge :: (ValidGraph et n nl el) => Context (AdjType et) n nl el
+         -> Graph et n nl el -> Graph et n nl el
+merge ctxt g = Gr nm' em' nextE'
   where
     n = cNode ctxt
 
@@ -280,3 +291,76 @@ merge (toContext -> ctxt) g = Gr nm' em' nextE'
     nm' = foldl' (\m (v,es) -> M.adjust (second (`M.union`es)) v m)
                  (M.insert n (cLabel ctxt,adj) nm)
                  nAdj
+
+mergeAs :: (Mergeable et n nl el ctxt) => ctxt -> Graph et n nl el
+           -> Graph et n nl el
+mergeAs = merge . toContext
+
+-- -----------------------------------------------------------------------------
+
+type ValidGraph et n nl el = (Hashable n
+                             ,Eq n
+                             ,EdgeType et
+                             )
+
+insNode :: (ValidGraph et n nl el) => n -> nl -> Graph et n nl el -> Graph et n nl el
+insNode n l g = g { nodeMap = M.insert n (l, M.empty) (nodeMap g) }
+
+insEdge :: (ValidGraph et n nl el) => (n,n,el) -> Graph et n nl el
+           -> (Edge, Graph et n nl el)
+insEdge (u,v,l) g = (e, Gr nm' em' (succ e))
+  where
+    e = nextEdge g
+
+    nm' = addE u . addE v $ nodeMap g
+
+    addE = M.adjust (second $ M.insert e ())
+
+    em' = M.insert e (mkEdge u v, l) (edgeMap g)
+
+delNode :: (ValidGraph et n nl el) => n -> Graph et n nl el -> Graph et n nl el
+delNode n g = maybe g snd $ match n g
+
+delEdge :: (ValidGraph et n nl el) => Edge -> Graph et n nl el -> Graph et n nl el
+delEdge e g = g { nodeMap = foldl' (flip delE) (nodeMap g) ens
+                , edgeMap = M.delete e (edgeMap g)
+                }
+  where
+    ens = maybe [] (edgeNodes . fst) (M.lookup e (edgeMap g))
+
+    delE = M.adjust (second $ M.delete e)
+
+-- TODO: care about directionality of edge.
+delEdgeLabel :: (ValidGraph et n nl el, Eq el) => (n,n,el) -> Graph et n nl el
+                -> Graph et n nl el
+delEdgeLabel (u,v,l) g
+  | M.null es = g
+  | otherwise = g { nodeMap = delEs u . delEs v $ nm
+                  , edgeMap = em `M.difference` es
+                  }
+  where
+    nm = nodeMap g
+
+    em = edgeMap g
+
+    es = maybe M.empty (M.filter isE . M.intersection em . snd) $ M.lookup u nm
+
+    isE (et,el) = getNode (otherN u et) == v && el == l
+
+    delEs = M.adjust (second (`M.difference`es))
+
+delEdgesBetween :: (ValidGraph et n nl el) => n -> n -> Graph et n nl el
+                   -> Graph et n nl el
+delEdgesBetween u v g
+  | M.null es = g
+  | otherwise = g { nodeMap = delEs u . delEs v $ nm
+                  , edgeMap = em `M.difference` es
+                  }
+  where
+    nm = nodeMap g
+    em = edgeMap g
+    es = maybe M.empty (M.filter isE . M.intersection em . snd) $ M.lookup u nm
+
+    isE (et,_) = getNode (otherN u et) == v
+
+    delEs = M.adjust (second (`M.difference`es))
