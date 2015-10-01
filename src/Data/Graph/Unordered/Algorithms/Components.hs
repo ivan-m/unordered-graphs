6c80fd6526933d35fae50832a18b8d3c531956e3
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2015-10-01 16:32:52 +1000

    Can now get connected components

--- src/Data/Graph/Unordered/Algorithms/Components.hs
+++ src/Data/Graph/Unordered/Algorithms/Components.hs
@@ -10,9 +10,15 @@
  -}
 module Data.Graph.Unordered.Algorithms.Components where
 
-import Data.Graph.Unordered
+import           Data.Graph.Unordered
 
-import Data.List (unfoldr)
+import           Control.Arrow (first)
+import qualified Data.DList as DL
+import qualified Data.HashMap.Strict as HM
+import qualified Data.HashSet as HS
+import           Data.List (unfoldr,mapAccumL)
+import           Data.Maybe (catMaybes)
+import           Data.Tuple (swap)
 
 -- -----------------------------------------------------------------------------
 
@@ -27,4 +33,24 @@ getComponent g = uncurry getComponentFrom <$> matchAny g
 
 getComponentFrom :: (ValidGraph et n) => Context (AdjType et) n nl el
                     -> Graph et n nl el -> (Graph et n nl el, Graph et n nl el)
-getComponentFrom c g = undefined
+getComponentFrom c = first (buildGr . (c:) . DL.toList)
+                     . step (HS.singleton (cNode c)) (HS.fromList (adjN c))
+  where
+    step vis toVis g
+      | HS.null toVis = (mempty,g)
+      | otherwise     = first (DL.fromList cs`DL.append`) (step vis' toVis' g')
+      where
+        (g',mcs) = mapAccumL getC g (HS.toList toVis)
+
+        cs = catMaybes mcs
+
+        -- smaller set should be first for good performance
+        vis' = toVis `HS.union` vis
+
+        toVis' = (`HS.difference`vis')
+                 . HS.fromList
+                 . concatMap adjN
+                 $ cs
+
+    getC g n = maybe (g,Nothing) (swap . first Just) (match n g)
+    adjN = map (getNode . fst) . HM.elems . cAdj
